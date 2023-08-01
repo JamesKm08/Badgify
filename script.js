41b9c5d9c36a3acd44140fbd576babe7d68ffde7
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-08-01 14:52:39 +0300

    Project Updates

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ let displayScreen = (data) => {
   data.map(i =>  {
     let entry = document.createElement('songDiv');
     entry.innerHTML = `
-      <img src = ${i.image} alt = 'image for playlist' width="200" height="200">
+      <img src = ${i.image} alt = 'image for playlist' width="100" height="80">
+      <h3>By: ${i.author}</h3>
       <h3>Genre: ${i.genre}</h3>
       <h3>${i.description}</h3>
-      <
+      <button class="play-btn" data-id="${i.id}">Play Playlist </button>
     `
     playlist.appendChild(entry)
  
